61ec97ec7903a7b647322cd117b4e3a251874d0b
dawon <ekdnjs210@gmail.com>
2023-10-03 20:36:20 +0900

    error fix

--- src/main/java/com/fastcampus/projectboard/FastcampusProjectBoardApplication.java
+++ src/main/java/com/fastcampus/projectboard/FastcampusProjectBoardApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FastcampusProjectBoardApplication {
+public class FastCampusProjectBoardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FastcampusProjectBoardApplication.class, args);
+//        SpringApplication.run(FastCampusProjectBoardApplication.class, args);
+
     }
 
 }
