5302d2d47ef8aa94d550d248cc0ef528175ceb16
dawon <ekdnjs210@gmail.com>
2023-10-11 22:22:03 +0900

    temp commit

--- src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
+++ src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
@@ -2,13 +2,14 @@ package com.fastcampus.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastCampusProjectBoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FastCampusProjectBoardApplication.class, args);
-
     }
 
 }
